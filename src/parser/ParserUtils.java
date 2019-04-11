c7d2b56f41276d1d4ad5f5eec3e14c1ed39b5a01
amirmhp98 <amir.blueb1377@gmail.com>
2019-04-11 17:13:33 +0430

    parser utils and some of it's methods added.

--- src/parser/ParserUtils.java
+++ src/parser/ParserUtils.java
@@ -1,8 +1,36 @@
 package parser;
 
+import java.util.Arrays;
+import java.util.List;
+
 /**
  * Created by amirmhp on 4/11/2019.
  */
 public class ParserUtils {
+
+    /**
+     * also think about linings while you remove comments
+     */
+    protected String removeComment(String inputText){
+        //todo implement
+        return null;
+    }
+
+    protected List<String> seprateLines(String inputText){
+        return Arrays.asList(inputText.split("\\n"));
+    }
+
+    protected List<String> splitByWhitespaces(String inputText){
+        //todo implement
+        return null;
+    }
+
+    /**
+     * it's important to put the symbols in result list
+     */
+    protected List<String> splitBySymbols(String inputText){
+        //todo implement
+        return null;
+    }
 
 }
